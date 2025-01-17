6b732b7d94719e3e897074d50f2426b3ded1e20e
koushalmodi <koushal.modi2001@gmail.com>
2025-01-17 17:04:29 +0530

    remomve gem

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
-gem 'gosu'
-
+# gem 'gosu'
 ruby '3.3.6'
-
 gem 'sinatra', '2.2.2'
 gem 'puma',    '5.6.5'
--- game.rb
+++ game.rb
@@ -1,91 +1,91 @@
-require 'gosu'
+# require 'gosu'
 require 'sinatra'
-
 get '/' do
-class Player
-  attr_reader :x, :y, :width, :height
+# class Player
+#   attr_reader :x, :y, :width, :height
 
-  def initialize
-    @x = 320
-    @y = 400
-    @width = 50
-    @height = 50
-    @speed = 5
-  end
+#   def initialize
+#     @x = 320
+#     @y = 400
+#     @width = 50
+#     @height = 50
+#     @speed = 5
+#   end
 
-  def move_left
-    @x -= @speed if @x > 0
-  end
+#   def move_left
+#     @x -= @speed if @x > 0
+#   end
 
-  def move_right
-    @x += @speed if @x < 640 - @width
-  end
+#   def move_right
+#     @x += @speed if @x < 640 - @width
+#   end
 
-  def draw
-    Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::BLUE, 1)
-  end
-end
+#   def draw
+#     Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::BLUE, 1)
+#   end
+# end
 
-class Star
-  attr_reader :x, :y, :size
+# class Star
+#   attr_reader :x, :y, :size
 
-  def initialize
-    @x = rand(640)
-    @y = 0
-    @size = 20
-    @speed = rand(2..5)
-  end
+#   def initialize
+#     @x = rand(640)
+#     @y = 0
+#     @size = 20
+#     @speed = rand(2..5)
+#   end
 
-  def fall
-    @y += @speed
-  end
+#   def fall
+#     @y += @speed
+#   end
 
-  def off_screen?
-    @y > 480
-  end
+#   def off_screen?
+#     @y > 480
+#   end
 
-  def draw
-    Gosu.draw_rect(@x, @y, @size, @size, Gosu::Color::YELLOW, 1)
-  end
-end
+#   def draw
+#     Gosu.draw_rect(@x, @y, @size, @size, Gosu::Color::YELLOW, 1)
+#   end
+# end
 
-class GameWindow < Gosu::Window
-  def initialize
-    super 640, 480
-    self.caption = "Ruby Gosu Game - Catch the Stars!"
+# class GameWindow < Gosu::Window
+#   def initialize
+#     super 640, 480
+#     self.caption = "Ruby Gosu Game - Catch the Stars!"
     
-    @player = Player.new
-    @stars = []
-    @score = 0
-    @font = Gosu::Font.new(20)
-  end
+#     @player = Player.new
+#     @stars = []
+#     @score = 0
+#     @font = Gosu::Font.new(20)
+#   end
 
-  def update
-    @player.move_left if Gosu.button_down?(Gosu::KB_LEFT)
-    @player.move_right if Gosu.button_down?(Gosu::KB_RIGHT)
+#   def update
+#     @player.move_left if Gosu.button_down?(Gosu::KB_LEFT)
+#     @player.move_right if Gosu.button_down?(Gosu::KB_RIGHT)
 
-    # Generate new stars randomly
-    @stars.push(Star.new) if rand(100) < 2
+#     # Generate new stars randomly
+#     @stars.push(Star.new) if rand(100) < 2
 
-    @stars.each(&:fall)
-    @stars.reject! do |star|
-      if (star.x - @player.x).abs < 30 && (star.y - @player.y).abs < 30
-        @score += 10
-        true
-      elsif star.off_screen?
-        true
-      else
-        false
-      end
-    end
-  end
+#     @stars.each(&:fall)
+#     @stars.reject! do |star|
+#       if (star.x - @player.x).abs < 30 && (star.y - @player.y).abs < 30
+#         @score += 10
+#         true
+#       elsif star.off_screen?
+#         true
+#       else
+#         false
+#       end
+#     end
+#   end
 
-  def draw
-    @player.draw
-    @stars.each(&:draw)
-    @font.draw_text("Score: #{@score}", 10, 10, 1, 1, 1, Gosu::Color::WHITE)
-  end
-end
+#   def draw
+#     @player.draw
+#     @stars.each(&:draw)
+#     @font.draw_text("Score: #{@score}", 10, 10, 1, 1, 1, Gosu::Color::WHITE)
+#   end
+# end
 
-GameWindow.new.show
+# GameWindow.new.show
+"hello"
 end
